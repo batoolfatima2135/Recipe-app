9c8d4a3a1e4cf462700290b516443cc1ad5522f3
Batool Fatima <batoolfatima2135@gmail.com>
2023-10-10 00:40:00 +0500

    Update default value

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_09_114702) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_09_154238) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,8 +37,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_09_114702) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.integer "preparation_time"
-    t.integer "cooking_time"
+    t.decimal "preparation_time", precision: 10, scale: 2
+    t.decimal "cooking_time", precision: 10, scale: 2
     t.text "description"
     t.boolean "public", default: false
     t.bigint "user_id", null: false
