15566a4418077ab53a63b94836b1ae26c2533852
Batool Fatima <batoolfatima2135@gmail.com>
2023-10-10 21:02:36 +0500

    Add model test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   it 'is valid with valid attributes' do
+    user = User.new(email: 'user@example.com', password: 'password123')
+    expect(user).to be_valid
+  end
+
+  it 'is not valid without an email' do
+    user = User.new(password: 'password123')
+    expect(user).not_to be_valid
+  end
+
+   it 'authenticates a user with a valid password' do
+    user = User.create(email: 'user@example.com', password: 'password123')
+    expect(user.valid_password?('password123')).to be true
+  end
+
+  it 'does not authenticate a user with an invalid password' do
+    user = User.create(email: 'user@example.com', password: 'password123')
+    expect(user.valid_password?('invalid_password')).to be false
+  end
 end
