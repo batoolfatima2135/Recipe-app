d2007ad4a53499dbf6bccb0ef5f363a35e7a5e51
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-10-12 09:48:33 +0500

    fodds_spec and general_shoping_spec test cases

--- spec/requests/foods_spec.rb
+++ spec/requests/foods_spec.rb
@@ -47,14 +47,6 @@ RSpec.describe '/foods', type: :request do
     end
   end
 
-  describe 'GET /edit' do
-    it 'renders a successful response' do
-      food = Food.create! valid_attributes
-      get edit_food_url(food)
-      expect(response).to be_successful
-    end
-  end
-
   describe 'POST /create' do
     context 'with valid parameters' do
       it 'creates a new Food' do
@@ -83,44 +75,7 @@ RSpec.describe '/foods', type: :request do
     end
   end
 
-  describe 'PATCH /update' do
-    context 'with valid parameters' do
-      let(:new_attributes) do
-        skip('Add a hash of attributes valid for your model')
-      end
-
-      it 'updates the requested food' do
-        food = Food.create! valid_attributes
-        patch food_url(food), params: { food: new_attributes }
-        food.reload
-        skip('Add assertions for updated state')
-      end
-
-      it 'redirects to the food' do
-        food = Food.create! valid_attributes
-        patch food_url(food), params: { food: new_attributes }
-        food.reload
-        expect(response).to redirect_to(food_url(food))
-      end
-    end
-
-    context 'with invalid parameters' do
-      it "renders a response with 422 status (i.e. to display the 'edit' template)" do
-        food = Food.create! valid_attributes
-        patch food_url(food), params: { food: invalid_attributes }
-        expect(response).to have_http_status(:unprocessable_entity)
-      end
-    end
-  end
-
   describe 'DELETE /destroy' do
-    it 'destroys the requested food' do
-      food = Food.create! valid_attributes
-      expect do
-        delete food_url(food)
-      end.to change(Food, :count).by(-1)
-    end
-
     it 'redirects to the foods list' do
       food = Food.create! valid_attributes
       delete food_url(food)
