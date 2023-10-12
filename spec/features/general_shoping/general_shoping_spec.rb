dd427c036003e2a8a00587dd9d1e5e8dd7d9dfbd
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-10-12 15:14:07 +0500

    integration test for general shoping list

---
+++ spec/features/general_shoping/general_shoping_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.feature 'General Shoping', type: :feature do
+  let(:user) { User.create!(email: 'testing@gmail.com', password: 'f4k3p455w0rd') }
+  let!(:foods) do
+    [
+      create(:food, name: 'Rice', measurement_unit: 'kg', price: 15, quantity: -3, user:),
+      create(:food, name: 'Beef', measurement_unit: 'kg', price: 100, quantity: -10, user:)
+    ]
+  end
+
+  before do
+    login_as(user, scope: :user)
+    visit general_shoping_list_path
+  end
+
+  scenario 'User views the General Shoping page' do
+    expect(page).to have_content('General Shoping List')
+    expect(page).to have_content('Amout Of Food Items to buy:')
+    expect(page).to have_content('Total value of food:')
+    expect(page).to have_css('table.table.table-success')
+  end
+
+  scenario 'User view the General Shoping list' do
+    foods.each do |food|
+      food.quantity = food.quantity.negative? ? food.quantity.abs : food.quantity
+      expect(page).to have_content(food.name)
+      expect(page).to have_content("#{food.quantity} #{food.measurement_unit}")
+      expect(page).to have_content("$#{food.price * food.quantity}")
+    end
+  end
+  scenario 'User clicks "Back to recipes" and is redirected to the recipes page' do
+    click_link 'Back to recipes'
+    expect(page).to have_current_path(recipes_path)
+  end
+end
