a83da5256a425fc74c710e5bbcc980618a7c4b4d
Batool Fatima <batoolfatima2135@gmail.com>
2023-10-12 20:57:18 +0500

    Add model tests

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { User.create!(email: 'model@gmail.com', password: 'f4k3p455w0rd') }
+
+  it 'is valid with valid attributes' do
+    food = Food.create(name: 'Rice', measurement_unit: 'kg', price: 15, quantity: -3, user: user)
+    expect(food).to be_valid
+  end
+
+  it 'is not valid without name' do
+    food = Food.create(measurement_unit: 'kg', price: 15, quantity: -3, user: user)
+    expect(food).not_to be_valid
+  end
+
+  it 'is not valid without measurement_unit' do
+    food = Food.create(name: 'Rice', price: 15, quantity: -3, user: user)
+    expect(food).not_to be_valid
+  end
+
+  it 'is not valid without price' do
+    food = Food.create(name: 'Rice', measurement_unit: 'kg', quantity: -3, user: user)
+    expect(food).not_to be_valid
+  end
+
+  it 'is not valid without quantity' do
+    food = Food.create(name: 'Rice', measurement_unit: 'kg', price: 15, user: user)
+    expect(food).not_to be_valid
+  end
+
+  it 'is not valid without user' do
+    food = Food.create(name: 'Rice', measurement_unit: 'kg', price: 15, quantity: -3)
+    expect(food).not_to be_valid
+  end
 end
